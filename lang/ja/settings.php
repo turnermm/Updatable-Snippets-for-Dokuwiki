332c7aee6a3e9d059667a5966582c69f43d65f50
Hideaki SAWADA <chuno@live.jp>
2015-03-17 21:10:06 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*
+* @author Hideaki SAWADA <chuno@live.jp>
+*/
+ 
+$lang['snippets_page'] = 'テンプレート一覧作成用 wiki ページ（順なし一覧が必須）';
+$lang['snips_updatable'] = '更新可スニペットをデフォルトとする';
