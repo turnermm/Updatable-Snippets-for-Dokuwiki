adba81a010bd12cf7230f2121192244f5aee7745
Hideaki SAWADA <chuno@live.jp>
2015-03-17 21:09:10 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*
+* @author Hideaki SAWADA <chuno@live.jp>
+*/
+ 
+$lang['js']['preview'] = 'プレビュー';
+$lang['js']['insert']  = '挿入';
+$lang['gb_snippets']   = 'テンプレートの挿入';
+$lang['popup_title']   = 'スニペット（テンプレート管理）';
+$lang['js']['check_for_updates'] = '更新可を選択';
+$lang['refresh_title'] = 'データベースから未使用のスニペット登録とページ ID を削除する';
+$lang['refresh'] = 'スニペットのデータベースを更新する';
+$lang['hide_table'] = '更新一覧を非表示にする';
+$lang['click_to_update'] = '更新をクリックする';
+$lang['js']['keepopen'] = 'ウインドウを閉じない';
