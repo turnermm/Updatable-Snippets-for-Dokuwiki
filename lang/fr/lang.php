feb038232498f5f654676b6ec237b54807ab5ad7
Floriang <antispam@floriang.eu>
2015-07-29 01:10:33 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Floriang <antispam@floriang.eu>
+ */
+$lang['js']['preview']         = 'Aperçu';
+$lang['js']['insert']          = 'Insérer';
+$lang['js']['check_for_updates'] = 'Effectuer une sélection actualisable';
+$lang['js']['keepopen']        = 'Laisser la fenêtre ouverte lors de la sélection';
+$lang['gb_snippets']           = 'Insérer modèles';
+$lang['popup_title']           = 'Snippets (gestionnaire de modèles)';
+$lang['refresh_title']         = 'Supprimer les entrées de snippet et identifiants de page inutilisés de la base de données';
+$lang['refresh']               = 'Rafraichir la base de données de snippets';
+$lang['hide_table']            = 'Cacher le tableau des mises à jour';
+$lang['click_to_update']       = 'Cliquer pour mettre à jour';
