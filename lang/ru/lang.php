c895840e32c7459587a49fb8e4aabeb39d196bbf
Aleksandr Selivanov <alexgearbox@yandex.ru>
2015-09-22 14:22:28 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['js']['preview']         = 'Предварительный просмотр';
+$lang['js']['insert']          = 'Вставить';
+$lang['js']['keepopen']        = 'Не закрывать окно после выбора';
+$lang['gb_snippets']           = 'Вставка шаблонов';
+$lang['popup_title']           = 'Сниппеты (управление шаблонами)';
+$lang['refresh']               = 'Обновить базу данных сниппетов';
+$lang['click_to_update']       = 'Нажмите для обновления';
