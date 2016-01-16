428ae9e70f46e242a69730cdd0d92c79ef3b68aa
Myeongjin <aranet100@gmail.com>
2016-01-16 07:49:15 +0100

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -4,14 +4,15 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Erial <erial2@gmail.com>
+ * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['js']['preview']         = '미리 보기';
-$lang['js']['insert']          = '집어넣기';
-$lang['js']['check_for_updates'] = '선택 내용 갱신 가능하게';
+$lang['js']['insert']          = '넣기';
+$lang['js']['check_for_updates'] = '선택한 것을 업데이트할 수 있도록 하기';
 $lang['js']['keepopen']        = '선택할 때 열어 놓은 창을 유지하기';
-$lang['gb_snippets']           = '템플릿 집어넣기';
-$lang['popup_title']           = '스니펫 (템플릿 매니저)';
-$lang['refresh_title']         = '데이터베이스에서 사용하지 않는 스니펫 등록 내용과 페이지 ID를 삭제';
-$lang['refresh']               = '스니펫 데이터베이스 새로고침';
-$lang['hide_table']            = '갱신 표 숨기기';
-$lang['click_to_update']       = '갱신 하기';
+$lang['gb_snippets']           = '템플릿 넣기';
+$lang['popup_title']           = '스니펫 (템플릿 관리자)';
+$lang['refresh_title']         = '데이터베이스에서 사용하지 않는 스니펫 	항목과 페이지 ID를 제거';
+$lang['refresh']               = '스니펫 데이터베이스 새로 고침';
+$lang['hide_table']            = '업데이트 표 숨기기';
+$lang['click_to_update']       = '업데이트하려면 클릭';
