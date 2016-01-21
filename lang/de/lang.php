14f2edb6f687638d4e27e171bf4619e00b80d509
liz <marliza@web.de>
2016-01-21 11:45:32 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,10 +4,15 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Padhie <develop@padhie.de>
+ * @author liz <marliza@web.de>
  */
 $lang['js']['preview']         = 'Vorschau';
 $lang['js']['insert']          = 'Einfügen';
-$lang['js']['keepopen']        = 'Fenster bleibt wörend der auswahl offen';
+$lang['js']['check_for_updates'] = 'Auswahl aktualisierbar machen';
+$lang['js']['keepopen']        = 'Fenster bleibt während der Auswahl offen';
 $lang['gb_snippets']           = 'Vorlagen einfügen';
 $lang['popup_title']           = 'Snippets (Vorlagen Manager)';
+$lang['refresh_title']         = 'Nicht verwendete unvollständige Einträge (Snippets) und Seiten-IDs aus Datenbank entfernen';
+$lang['refresh']               = 'Aktualisiere Snippet Datenbank';
+$lang['hide_table']            = 'Updates-Tabelle ausblenden';
 $lang['click_to_update']       = 'Klicke zum aktualisieren';
