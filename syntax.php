6a36ed54ce45be7f232dfafdc8c2f57303494d69
Myron Turner <turnermm02@shaw.ca>
2016-02-02 13:10:21 -0600

    php 7.0 signature fixes

--- syntax.php
+++ syntax.php
@@ -37,7 +37,7 @@ class syntax_plugin_snippets extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler){
 
         switch ($state) {
             case DOKU_LEXER_ENTER:
@@ -60,7 +60,7 @@ class syntax_plugin_snippets extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $indata) {
+    function render($mode, Doku_Renderer $renderer, $indata) {
         list($instr, $data) = $indata;
         if($mode == 'xhtml'){
             switch ($instr) {
