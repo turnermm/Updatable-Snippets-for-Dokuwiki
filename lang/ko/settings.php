2bda3c76d62f21f087b8e345d536321dcaa32b45
Myeongjin <aranet100@gmail.com>
2016-03-22 08:05:17 +0100

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -6,5 +6,5 @@
  * @author Erial <erial2@gmail.com>
  * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['snippets_page']         = '템플릿 목록을 생성할 위키 문서 (순서 없는 목록을 포함되어야 합니다)';
+$lang['snippets_page']         = '템플릿 목록을 생성할 위키 문서 (순서 없는 목록을 포함해야 합니다)';
 $lang['snips_updatable']       = '업데이트할 수 있는 스니펫을 기본 모드로 설정';
