b03372bd4e664cd4bd819861e953ab483a7669ef
Marcel Bachus <marcel.bachus@ziggo.nl>
2016-08-18 12:05:05 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marcel Bachus <marcel.bachus@ziggo.nl>
+ */
+$lang['snippets_page']         = 'Wiki pagina om template lijst te genereren van (deze moet een ongeordende lijst bevatten)';
+$lang['snips_updatable']       = 'Maak veranderbare snippets als standaard';
