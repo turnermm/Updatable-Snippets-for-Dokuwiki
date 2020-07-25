015a06ce43ecdcedcdfadefeb4e5978c08a83b0f
Axel Schwarzer <SchwarzerA@gmail.com>
2020-07-25 17:20:06 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,9 +3,14 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)  
  *
+ * @author Axel Schwarzer <SchwarzerA@gmail.com>
  * @author liz <marliza@web.de>
  * @author Umberto Schlansky <schohns@quantentunnel.de>
  */
 $lang['snippets_page']         = 'Wikiseite, aus der die Vorlagenliste erstellt wird (muss eine ungeordnete Liste enthalten)';
 $lang['snips_updatable']       = 'Snippets standardmäßig aktualisierbar machen';
 $lang['old_revisions']         = 'Lade automatisch neue Snippetsversionen in  Ältere Versionen';
+$lang['userreplace']           = 'Ersetzen Sie benutzerdefinierte Makros.';
+$lang['prettytitles']          = 'Ersetzen Sie Unterstriche durch Leerzeichen in Ersetzungen für Seitentitel.';
+$lang['skip_unset_macros']     = 'Entfernen Sie Makros ohne Substitutionswerte.';
+$lang['default_macro_string']  = 'Eine benutzerdefinierte Standardmakrozeichenfolge für das Textfeld <b>Benutzermakros & Ersetzungen</b>.';
